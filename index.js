09e05f898a91df63cafac97ad01f46302bc191f5
Konstantin Klitenik <kostyan5@gmail.com>
2021-02-11 16:24:47 -0500

    Add deleted users slack report

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
 const { WebClient, LogLevel } = require("@slack/web-api");
 
-function findDeleted(userList, sinceDaysAgo) {
-  deletedUsers = userList.filter(u => u.deleted);
+const client = new WebClient(process.env.SLACK_TOKEN, {
+  logLevel: LogLevel.INFO,
+});
+
+async function findDeleted(userList, sinceDaysAgo = 1) {
+  const updatedCutoff = (Date.now() / 1000 ) - sinceDaysAgo * 24 * 3600 - 3600; // timestamp in seconds (subtract an hour to overlap)
+
+  deletedUsers = userList.filter(u => u.deleted && u.updated > updatedCutoff);
   deletedUsers.sort((a, b) => b.updated - a.updated); // sort in reverse chron order by updated time
 
   deletedUsers = deletedUsers.map(u => {
@@ -13,20 +19,31 @@ function findDeleted(userList, sinceDaysAgo) {
     };
   });
 
-  console.log('deleted :>> ', deletedUsers.slice(0, 10));
+  // console.log('deleted :>> ', deletedUsers.slice(0, 5));
+
+  let message = 'No one deactivated today';
+  if (deletedUsers.length > 0) {
+    message = deletedUsers.map(u => `${u.realName} (${u.name}) deactivated on ${u.updated}`).join('\n')
+  }
+
+  const result = await client.chat.postMessage({
+    channel: process.env.SLACK_USER_ID,
+    text: `\`\`\`${message}\`\`\``,
+  });
+
+  // console.log(result);
+
+  return deletedUsers;
 }
 
 async function run() {
   console.log("Kon slack bot");
 
-  const client = new WebClient(process.env.SLACK_TOKEN, {
-    logLevel: LogLevel.DEBUG
-  });
-
   try {
     // Call the users.list method using the WebClient
     const result = await client.users.list();
-    // console.log('result :>> ', result);
+    // console.log('result :>> ', result.members.filter(m => m.profile.real_name.includes('Konstantin')));
+    
     findDeleted(result.members, 1);
 
   }
